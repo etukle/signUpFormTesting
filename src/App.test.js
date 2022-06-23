2284bff55fb6b4a46a0d5688cfd306ba0dfec532
Evren Tukle <tukleevren@gmail.com>
2022-06-23 13:54:25 +0300

    signUp formm elements tested and codded

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("inputs should be initially empty", () => {
+  render(<App />)
+  const emailInput = screen.getByRole("textbox")
+  const passwordInput = screen.getByLabelText("Password")
+  const confirmPasswordInput = screen.getByLabelText("Confirm Password")
+  expect(emailInput.value).toBe("")
+  expect(passwordInput.value).toBe("")
+  expect(confirmPasswordInput.value).toBe("")
+})
+
+test("should be able to type an email", () => {
+  render(<App />)
+  const emailInput = screen.getByRole("textbox", {name: /email/i})
+  userEvent.type(emailInput, "test@test.com")
+  expect(emailInput.value).toBe("test@test.com")
+})
